b053f7566e8b7f181776b61e9695be10d7170ce3
unknown <Francesco@.(none)>
2013-04-21 23:30:46 +0200

    update

---
+++ Examples/service.py
@@ -0,0 +1,11 @@
+import urllib,urllib2
+import json
+API_URL="http://www.scurtu.it/apis/documentSimilarity"
+inputDict={}
+inputDict['doc1']='Document with some text'
+inputDict['doc2']='Other document with some text'
+params = urllib.urlencode(inputDict)    
+f = urllib2.urlopen(API_URL, params)
+response= f.read()
+responseObject=json.loads(response)  
+print responseObject
